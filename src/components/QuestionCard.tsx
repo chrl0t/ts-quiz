78a528452626c854e57d82f5596ae6e681b405e8
chrl0t <charlotte_i@live.co.uk>
2021-05-12 15:39:51 +0100

    Functionality for question card component

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 
-const QuestionCard = () => {
-  <div>Question Card</div>;
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNo: number;
+  totalQuestions: number;
 };
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNo,
+  totalQuestions
+}) => (
+  <div>
+    <p className='number'>
+      Question: {questionNo} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <div>
+          <button disabled={userAnswer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 export default QuestionCard;
